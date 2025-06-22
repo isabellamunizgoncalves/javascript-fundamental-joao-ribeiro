924213c0462ec813f9da98f631b9ca1e04ae5eba
Isabella Gonçalves <isabella.goncalves@cyberprotech.pt>
2025-06-22 22:02:14 +0100

    loop-exercises-06

--- loops-iterações/ex_06.js
+++ loops-iterações/ex_06.js
@@ -5,10 +5,19 @@
 
 let names = ["John", "Paul", "Ringo", "George"];
 
+//1 forma
 for(let i = 0; i < names.length; i++)
 {
     if(names[i] === "Paul"){
         continue;
     }
     console.log(names[i]);
+}
+
+//2 forma
+for(let name of names){
+    if(name == 'Paul'){
+        continue;
+    }
+    console.log(name);
 }
