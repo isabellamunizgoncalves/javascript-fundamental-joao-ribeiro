be0601829d583dd7c50e3d48741102523c890a26
Isabella Gonçalves <isabella.goncalves@cyberprotech.pt>
2025-06-22 15:26:31 +0100

    Exercicio loops 02

---
+++ loops-iterações/ex_02.js
@@ -0,0 +1,14 @@
+/**
+ * Defina uma variavel number com valor 1000.
+ * Crie um loop for que se repete 10 vezes.
+ * A cada iteração do loop, incremente o valor de number em 100.
+ * mostre o valor de cada number no console
+ */
+
+let number = 1000;
+
+for(let i = 0; i <= 10; i++)
+{
+    console.log(number);
+    number += 100;
+}
