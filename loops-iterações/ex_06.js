adf7ae559a7fb97a535286fb7b4e23b7b053cbe5
Isabella Gonçalves <isabella.goncalves@cyberprotech.pt>
2025-06-22 21:56:17 +0100

    loop-exercises-06

---
+++ loops-iterações/ex_06.js
@@ -0,0 +1,14 @@
+/**
+ * Crie um array com 4 nomes: John, Paul, Ringo e George
+ * Crie um loop que mostre os nomes no console exceto o nome Paul.
+ */
+
+let names = ["John", "Paul", "Ringo", "George"];
+
+for(let i = 0; i < names.length; i++)
+{
+    if(names[i] === "Paul"){
+        continue;
+    }
+    console.log(names[i]);
+}
