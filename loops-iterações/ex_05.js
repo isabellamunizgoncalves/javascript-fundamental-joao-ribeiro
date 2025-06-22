8c60c480e32350cd9413ffb92ed6210d9f99ecd7
Isabella Gonçalves <isabella.goncalves@cyberprotech.pt>
2025-06-22 21:47:39 +0100

    loop-exercises-05

---
+++ loops-iterações/ex_05.js
@@ -0,0 +1,11 @@
+/**
+ * Crie um loop for que mostre os números de 0 a 10 no console.
+ * Mas que termine quando o número 5 for encontrado
+ */
+
+for(let i = 0; i <= 10; i++){
+    if(i === 5){
+        break;
+    }
+    console.log(i);
+}
