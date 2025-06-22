c22364692353b5667ee2538a1a8e64c0cb63abf9
Isabella Gonçalves <isabella.goncalves@cyberprotech.pt>
2025-06-22 15:19:31 +0100

    Exercicio 01

---
+++ loops-iterações/ex_01.js
@@ -0,0 +1,12 @@
+/**
+ * Crie um loop for para exibir a tabuada do valor 6
+ * Exemplo: 6 x 1 = 6
+ */
+
+
+//tabuada do 6 no ciclo for
+for(let i = 1; i <= 10; i++)
+{
+    resultado = i * 6;
+    console.log("6 X ", i, " = ", resultado);
+}
