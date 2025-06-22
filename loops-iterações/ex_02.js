ed75391261f287975631bbe3398970cd42ca958e
Isabella Gonçalves <isabella.goncalves@cyberprotech.pt>
2025-06-22 15:30:08 +0100

    Exercicio loops 03 while

--- loops-iterações/ex_02.js
+++ loops-iterações/ex_02.js
@@ -7,7 +7,7 @@
 
 let number = 1000;
 
-for(let i = 0; i <= 10; i++)
+for(let i = 0; i < 10; i++)
 {
     console.log(number);
     number += 100;
---
+++ loops-iterações/ex_03.js
@@ -0,0 +1,10 @@
+/**
+ * Crie um loop while que mostre os números de 0 a 10 no console.
+ */
+
+let i = 0;
+
+while( i <= 10){
+    console.log(i);
+    i++;
+}
