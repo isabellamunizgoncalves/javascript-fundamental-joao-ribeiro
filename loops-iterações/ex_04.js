2d02f82d4b924db10e6bebd0099dd0d49b220a4c
Isabella Gonçalves <isabella.goncalves@cyberprotech.pt>
2025-06-22 15:33:35 +0100

    Exercicio loops 04 do while

---
+++ loops-iterações/ex_04.js
@@ -0,0 +1,11 @@
+/**
+ * Crie um loop do while que mostre os numeros de 0 a 100 no console,
+ * com incrementos de 10 em 10;
+ */
+
+let i = 0;
+
+do{
+    console.log(i);
+    i += 10;
+}while(i <= 100);
