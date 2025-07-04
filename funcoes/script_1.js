77813ce25b666d55f60bb950a1575ae682f7ff00
isabellamunizgoncalves <contato.isabellaramos@hotmail.com>
2025-07-04 09:02:20 +0100

    Módulo Funções

---
+++ funcoes/script_1.js
@@ -0,0 +1,13 @@
+/*
+ * Vamos criar uma função que recebe dois 
+ * números e retorna a soma deles.
+ */
+
+function addNumbers(num1, num2){
+    console.log(num1 + num2);
+}
+
+let number1 = 10;
+let number2 = 120;
+
+addNumbers(number1, number2);
